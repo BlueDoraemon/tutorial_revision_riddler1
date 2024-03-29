472d4c82fb383d93f3913b2b757237c1f0e9ed61
BlueDoraemon <gtlx54@gmail.com>
2024-03-29 21:23:31 +1100

    done

--- src/main/scala/cosc250/firststeps/Practice.scala
+++ src/main/scala/cosc250/firststeps/Practice.scala
@@ -1,12 +1,21 @@
 package cosc250.firststeps
+  import annotation.tailrec
 
 
 /** 
  * Rotate a list N places to the left. For instance 
  * rotate(3, List(1, 2, 3, 4, 5, 6)) would be List(4, 5, 6, 1, 2, 3)
  */
-def rotate[T](by:Int, list:List[T]):List[T] = 
-  ???
+
+def rotate[T](by:Int, list:List[T]):List[T] = {
+  @tailrec
+  def turn[T](by:Int, list:List[T]):List[T] = by match{
+    case 0 => list
+    case _ => turn(by -1, list.tail :+ list.head)
+  }
+  turn(by,list)
+}
+
 
 /**
  * Pack consecutive duplicates in a list into sublists
@@ -15,17 +24,29 @@ def rotate[T](by:Int, list:List[T]):List[T] =
  * would be List(List(1, 1, 1), List(2, 2), List(3), List(4, 4))
  * 
  */
-def pack[T](list:List[T]):List[List[T]] = 
-  ???
+def pack[T](list: List[T]): List[List[T]] = {
+  @tailrec
+  def subList(current: List[T], totalList: List[List[T]], currentSublist: List[T]): List[List[T]] = current match {
+    case Nil => totalList :+ currentSublist // Add the last sublist when we reach the end of the list
+    case head :: tail if currentSublist.isEmpty || head == currentSublist.head =>
+      subList(tail, totalList, currentSublist :+ head) // Continue with the current sublist
+    case head :: tail =>
+      subList(tail, totalList :+ currentSublist, List(head)) // Start a new sublist
+  }
+
+  if (list.isEmpty) Nil
+  else subList(list, Nil, Nil)
+}
+
   
 
 /**
  * Sort a list of lists according to the length of the sublist
- * 
+ *
  * For instance, sortByLength(List(List(1, 1, 1), List(2, 2), List(4)))
  * would be List(List(4), List(2, 2), List(1, 1, 1))
  */
-def sortByLength[T](outer:List[List[T]]):List[List[T]] = ???
+def sortByLength[T](outer:List[List[T]]):List[List[T]] = outer.sortBy(_.length)(Ordering[Int].reverse)
 
 /**
  * FoldLeft on a tree
@@ -36,27 +57,27 @@ def sortByLength[T](outer:List[List[T]]):List[List[T]] = ???
  *       are start and value.
  *       For Branch, call foldLeft on the left subtree, get the result, 
  *       and pass that into foldLeft on the right subtree.  
- */ 
+ */
 enum Tree[+T]:
   def foldLeft[B](start:B)(f: (B, T) => B):B = this match
-    case Empty => ???
-    case Branch(left, right) => ???
-    case Leaf(value) => ???
+  case Empty => ???
+  case Branch(left, right) => ???
+  case Leaf(value) => ???
 
   case Empty
   case Branch(left:Tree[T], right:Tree[T])
   case Leaf(value:T)
 
 
-extension (t:Tree[Int])
+  extension (t:Tree[Int])
   def sum:Int = t.foldLeft(0)(_ + _)
 
-  def printFold() = 
+  def printFold() =
     t.foldLeft(0)((i, x) => {
       println(s"$i. Value in this node is $x")
       i + 1
     })
 
-/** Just a main method for you to use as you write your code */
-@main def practiceMain = 
-  ???
+  /** Just a main method for you to use as you write your code */
+  @main def practiceMain =
+    ???
--- src/test/scala/cosc250/firststeps/TestSuite.scala
+++ src/test/scala/cosc250/firststeps/TestSuite.scala
@@ -2,8 +2,11 @@ package cosc250.firststeps
 
 class TestSuite extends munit.FunSuite {
 
-  test("This is only a placeholder test") {
-    assertEquals(true, true)
+  test("rotate should correctly rotate the list to the right by the specified number of elements") {
+    val list = List(1, 2, 3, 4, 5)
+    val expected = List(4, 5, 1, 2, 3)
+    val result = rotate(2, list)
+    assertEquals(result, expected)
   }
 
 
